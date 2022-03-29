d816e149fa0d19e3729331194f8ddb93840cfaa7
adarsh sharma <91409917+adarsh4code@users.noreply.github.com>
2022-03-29 07:48:03 -0700

    added a body and a method
    added a method as well as a body to the product.java class, which returns a product of given two numbers !

---
+++ product.java
@@ -0,0 +1,8 @@
+public class Product{
+public static void main(String [] args){
+
+}
+public static int productOfTwoNumbers(int firstNumber, int second Number){
+ return firstNumber*secondNumber;
+}
+}
